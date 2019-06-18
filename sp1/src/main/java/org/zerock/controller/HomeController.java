dcfed846b1210950ca65118acf6a6126f83936b5
Jee hoon AHN <doggyblanc@gmail.com>
2019-06-18 15:28:41 +0900

    initial config and junit environment ready

--- sp1/src/main/java/org/zerock/controller/HomeController.java
+++ sp1/src/main/java/org/zerock/controller/HomeController.java
@@ -6,10 +6,12 @@ import java.util.Locale;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.zerock.service.HelloService;
 
 /**
  * Handles requests for the application home page.
@@ -19,11 +21,20 @@ public class HomeController {
 	
 	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
 	
+	//타입은 임포트를 하지만 new를 통해 생성은 안한다.
+	@Autowired
+	private HelloService service;
+	
+	
 	/**
 	 * Simply selects the home view to render by returning its name.
 	 */
 	@RequestMapping(value = "/", method = RequestMethod.GET)
 	public String home(Locale locale, Model model) {
+		logger.info("---------------------------");
+		logger.info(""+service);
+		logger.info(service.toString());
+		logger.info("---------------------------");
 		logger.info("Welcome home! The client locale is {}.", locale);
 		
 		Date date = new Date();
